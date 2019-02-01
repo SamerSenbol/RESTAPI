004024360548dfbf3422d6fbd44219139aef42cc
SamerSenbol <samer.senbol@gmail.com>
2019-02-01 11:02:01 +0100

    Fixing the syntax.

--- script.js
+++ script.js
@@ -31,52 +31,26 @@ function getHoroscope(){
 }
 
 
-function deleteHoroscope()   {
-  //console.log('user_date'); 
-  //var formData = new FormData();
-
-//formData.append('user_date', document.getElementById("user_date").value);
-
+function deleteHoroscope() {
 fetch('deleteHoroscope.php', {
   method: 'DELETE',
-  //body: formData
 })
 .then(response => response.json())
-//.then(response => {
- 
-
-  //$('#table-id').append('<tr><td>'+response.firas+'</td></tr>');
-
-
-//})
 .catch(error => console.error('Error:', error));
- }
+}
 
 
-function updateHoroscope()   {
-  //console.log('user_date'); 
-  //var formData = new FormData();
-
-//formData.append('user_date', document.getElementById("user_date").value);
-
+function updateHoroscope() {
 fetch('updateHoroscope.php', {
-  method: 'PUT',
-  //body: formData
+  method: 'PUT'
 })
 .then(response => response.json())
-.then(response => {
- 
-
-  //$('#table-id').append('<tr><td>'+response.firas+'</td></tr>');
-
-
+.then(response => { 
 })
 .catch(error => console.error('Error:', error));
  } 
 
+//https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+//https://www.w3schools.com/jquery/html_append.asp
 
- //https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
- //https://www.w3schools.com/jquery/html_append.asp
 
-
-//////////////////////////////////////////////////////////////////////////
